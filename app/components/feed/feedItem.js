7cb77d6125ff773b6cc10a1c2ddfee11104d7ba8
Manish Gopal Singh <manish.singh@yipl.com.np>
2017-10-13 15:40:46 +0545

    add share modal in feed screen update modal share UI update group and frineds photo

--- app/components/feed/feedItem.js
+++ app/components/feed/feedItem.js
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import Group from './card/group';
 import Offer from './card/offer';
 
-const feedItem = ({ feed, onPress }) => {
+const feedItem = ({ feed, onPress, onSharePress }) => {
   if (feed.feedable === 'trip') {
-    return (<Offer onPress={onPress} offer={feed.Trip} />);
+    return (<Offer onPress={onPress} onSharePress={onSharePress} offer={feed.Trip} />);
   }
 
-  return (<Group onPress={onPress} group={feed.Group} />);
+  return (<Group onPress={onPress} onSharePress={onSharePress} group={feed.Group} />);
 };
 
 feedItem.propTypes = {
   feed: PropTypes.shape({
-    title: PropTypes.string,
-    body: PropTypes.string,
+    name: PropTypes.string,
   }).isRequired,
   onPress: PropTypes.func.isRequired,
+  onSharePress: PropTypes.func.isRequired,
 };
 
 export default feedItem;
