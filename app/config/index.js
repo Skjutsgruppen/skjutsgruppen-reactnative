5320d0b25a00313c93170c53eb359a862693ad20
Manish Gopal Singh <manish.singh@yipl.com.np>
2017-10-25 16:27:50 +0545

    add comment load more add comment subscription

--- app/config/index.js
+++ app/config/index.js
@@ -2,5 +2,6 @@ import Config from 'react-native-config';
 
 const API_URL = Config.API_URL;
 const GOOGLE_PLACE_KEY = Config.GOOGLE_PLACE_KEY;
+const WS_API_URL = Config.WS_API_URL;
 
-export { API_URL, GOOGLE_PLACE_KEY };
+export { API_URL, GOOGLE_PLACE_KEY, WS_API_URL };
