1fa994025e65445c356665c71163677bfc0600b7
Manish Gopal Singh <manish.singh@yipl.com.np>
2017-12-28 18:04:12 +0545

    map loading improvements

--- app/components/map/tripMarker.js
+++ app/components/map/tripMarker.js
@@ -1,31 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
 import Marker from '@components/map/marker';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Loading } from '@components/common';
 
-const { width, height } = Dimensions.get('window');
-const ASPECT_RATIO = width / height;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
-
-const TripMarker = ({ lat, lng, loading, trips, user, onMarkerPress }) => {
-  if (loading) return <Loading />;
+const TripMarker = ({ trips, onMarkerPress }) => {
   let coordinate = {};
-  const renderMarkers = trips.map((row) => {
+  if (trips.length < 1) {
+    return null;
+  }
+
+  return trips.map((row) => {
     coordinate = {
       latitude: row.coordinate.lat,
       longitude: row.coordinate.lng,
@@ -43,52 +26,12 @@ const TripMarker = ({ lat, lng, loading, trips, user, onMarkerPress }) => {
       />
     );
   });
-
-  return (
-    <View style={styles.container}>
-      <MapView
-        cacheEnabled
-        loadingEnabled
-        loadingIndicatorColor="#666666"
-        loadingBackgroundColor="#eeeeee"
-        style={styles.map}
-        region={
-          {
-            latitude: lat,
-            longitude: lng,
-            latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA,
-          }
-        }
-      >
-        <Marker
-          onPress={(e) => {
-            e.stopPropagation();
-          }}
-          coordinate={{
-            latitude: lat,
-            longitude: lng,
-          }}
-          image={user.avatar}
-          count={0}
-        />
-        {renderMarkers}
-      </MapView>
-    </View>
-  );
 };
 
 TripMarker.propTypes = {
   onMarkerPress: PropTypes.func.isRequired,
-  lat: PropTypes.number.isRequired,
-  lng: PropTypes.number.isRequired,
   trips: PropTypes.arrayOf(PropTypes.object).isRequired,
-  user: PropTypes.shape({
-    photo: PropTypes.string.isRequired,
-  }).isRequired,
   loading: PropTypes.bool.isRequired,
 };
 
-const mapStateToProps = state => ({ user: state.auth.user });
-
-export default connect(mapStateToProps)(TripMarker);
+export default TripMarker;
