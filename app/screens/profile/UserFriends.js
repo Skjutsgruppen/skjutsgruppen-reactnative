9bef36ca4d8db8e1ceb4cc8902ee0af82beed9b8
Aayush Rijal <reizal.ayz@gmail.com>
2018-01-05 14:39:19 +0545

    Friends of friends listed in modal - [x] onProfilePress fixes - [x] styled relation and relation modal - [x] friends of friends in comments - [x] Relation description in comments fixed - [x] Relation listing modal in profile - [x] UI fixes of friends of friends in comments - issue #93

--- app/screens/profile/UserFriends.js
+++ app/screens/profile/UserFriends.js
@@ -12,6 +12,45 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.background.lightGray,
     paddingBottom: 12,
   },
+  lightText: {
+    color: '#777777',
+  },
+  feed: {
+    backgroundColor: '#f9f9f9',
+    borderRadius: 8,
+    marginRight: 6,
+    marginLeft: 6,
+    marginBottom: 16,
+    borderColor: '#cccccc',
+    borderBottomWidth: 4,
+  },
+  feedContent: {
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  feedTitle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 12,
+  },
+  imgIcon: {
+    height: 55,
+    width: 55,
+    backgroundColor: '#ddd',
+    borderRadius: 36,
+    marginRight: 12,
+  },
+  name: {
+    color: '#1db0ed',
+    fontWeight: 'bold',
+  },
+  profilePic: {
+    height: 55,
+    width: 55,
+    borderRadius: 27,
+    marginRight: 12,
+  },
 });
 
 const Friends = withMyFriends(FriendsList);
