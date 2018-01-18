3215c4e8f6ed354203d6d32eef932634e350a36a
Manish Gopal Singh <manish.singh@yipl.com.np>
2018-01-18 16:29:56 +0545

    UI fixes of feed cards and refactor UI changes of experience card refactored froup feed card and changed default theme for texts added transportation icons and animation in public transportation card lists

--- app/components/feed/card/group.js
+++ app/components/feed/card/group.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 import PropTypes from 'prop-types';
 import { STRETCH_TYPE_AREA, STRETCH_TYPE_ROUTE, FEEDABLE_GROUP } from '@config/constant';
+import { Footer } from '@components/feed/card';
 import Colors from '@theme/colors';
 import GroupImage from '@components/group/groupImage';
-import ShareIcon from '@assets/icons/ic_share.png';
 import { trans } from '@lang/i18n';
 
 const cardHeight = 484;
 const profilePicSize = 60;
 
 const styles = StyleSheet.create({
-  flex1: {
-    flex: 1,
-  },
   wrapper: {
-    height: cardHeight,
+    maxHeight: cardHeight,
     backgroundColor: Colors.background.fullWhite,
     marginHorizontal: 16,
     marginVertical: 10,
@@ -73,15 +70,21 @@ const styles = StyleSheet.create({
     borderColor: Colors.border.white,
   },
   detail: {
-    paddingHorizontal: 24,
-    paddingTop: 24,
+    paddingHorizontal: 18,
+    paddingTop: 20,
+    marginBottom: 20,
   },
   comment: {
     flex: 1,
-    paddingHorizontal: 24,
-    paddingVertical: 12,
+    flexBasis: 50,
+    minHeight: 50,
+    paddingHorizontal: 18,
     marginTop: 'auto',
     overflow: 'hidden',
+  },
+  commentText: {
+    fontSize: 16,
+    lineHeight: 24,
   },
   commentGradientOverlay: {
     height: 24,
@@ -91,18 +94,6 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     marginHorizontal: 24,
-  },
-  footer: {
-    paddingTop: 24,
-    paddingBottom: 24,
-    paddingHorizontal: 24,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  commentIcon: {
-    flexDirection: 'row',
-    alignItems: 'center',
   },
   text: {
     lineHeight: 20,
@@ -114,15 +105,6 @@ const styles = StyleSheet.create({
     color: Colors.text.blue,
     fontWeight: 'bold',
     marginRight: 2,
-  },
-  readMore: {
-    color: Colors.text.blue,
-    fontWeight: 'bold',
-  },
-  shareIcon: {
-    width: 24,
-    height: 24,
-    resizeMode: 'contain',
   },
 });
 
@@ -141,55 +123,53 @@ const Group = ({ group, onPress, min, onSharePress, wrapperStyle }) => {
     <View style={[styles.wrapper, wrapperStyle]}>
       <TouchableWithoutFeedback
         onPress={() => onPress(FEEDABLE_GROUP, group)}
-        style={styles.flex1}
       >
-        <View style={styles.flex1}>
-          <GroupImage
-            group={group}
-            roundedCorner
-          />
-          <View style={styles.detail}>
-            <View>
-              <Text style={[styles.text, styles.lightText]}>
-                <Text style={styles.username}>
-                  {group.User.firstName}
-                </Text>
-                <Text> {trans('feed.created_a_group')}</Text>
-              </Text>
-              {
-                group.outreach === STRETCH_TYPE_AREA &&
+        <View>
+          <View style={styles.flex1}>
+            <GroupImage
+              group={group}
+              roundedCorner
+            />
+            <View style={styles.detail}>
+              <View>
                 <Text style={[styles.text, styles.lightText]}>
-                  {[group.country, group.county, group.municipality, group.locality].filter(s => s).join(', ')}
+                  <Text style={styles.username}>
+                    {group.User.firstName || group.User.email}
+                  </Text>
+                  <Text> {trans('feed.created_a_group')}</Text>
                 </Text>
-              }
-              {
-                group.outreach === STRETCH_TYPE_ROUTE &&
-                <Text style={[styles.text, styles.lightText]}>
-                  {group.TripStart.name} - {group.TripEnd.name}
-                </Text>
-              }
+                {
+                  group.outreach === STRETCH_TYPE_AREA &&
+                  <Text style={[styles.text, styles.lightText]}>
+                    {[group.country, group.county, group.municipality, group.locality].filter(s => s).join(', ')}
+                  </Text>
+                }
+                {
+                  group.outreach === STRETCH_TYPE_ROUTE &&
+                  <Text style={[styles.text, styles.lightText]}>
+                    {group.TripStart.name} - {group.TripEnd.name}
+                  </Text>
+                }
+              </View>
+            </View>
+            <View style={styles.comment}>
+              <Text style={[styles.text, styles.commentText]}>{group.description}</Text>
+              <View style={styles.commentGradientOverlay} />
             </View>
           </View>
-          <View style={styles.comment}>
-            <Text style={styles.text}>{group.description}</Text>
-            <View style={styles.commentGradientOverlay} />
+          <View style={styles.profilePicWrapper}>
+            {profileImage}
           </View>
           <View style={styles.profilePicWrapper}>
             {profileImage}
           </View>
         </View>
       </TouchableWithoutFeedback>
-      <View style={styles.footer}>
-        <TouchableOpacity onPress={() => onSharePress(FEEDABLE_GROUP, group)}>
-          <Image source={ShareIcon} style={styles.shareIcon} />
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.readMoreWrapper}
-          onPress={() => onPress(FEEDABLE_GROUP, group)}
-        >
-          <Text style={styles.readMore}>{trans('feed.read_more')}</Text>
-        </TouchableOpacity>
-      </View>
+      <Footer
+        onSharePress={() => onSharePress(FEEDABLE_GROUP, group)}
+        onCommentPress={() => onPress(FEEDABLE_GROUP, group)}
+        hasReadMore
+      />
     </View>
   );
 };
