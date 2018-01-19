f9882d69d8dede75cfc1cfa1a47c7fa2976a4447
Kiran Shrestha <kiran.shrestha@yipl.com.np>
2018-01-19 15:48:31 +0545

    - [x] Subscription for my trips when user comments/creates a trip. - [x] Display static map when trip is shared.

--- app/components/profile/tripsList.js
+++ app/components/profile/tripsList.js
@@ -1,41 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TripItem from '@components/profile/tripsItem';
 import PropTypes from 'prop-types';
 import DataList from '@components/dataList';
 
-const UserTripsList = ({ onPress, onSharePress, trips }) => (
-  <DataList
-    data={trips}
-    renderItem={({ item }) => (
-      <TripItem
-        key={item.id}
-        trip={item}
-        onPress={onPress}
-        onSharePress={onSharePress}
-      />
-    )}
-    fetchMoreOptions={{
-      variables: { offset: trips.rows.length },
-      updateQuery: (previousResult, { fetchMoreResult }) => {
-        if (!fetchMoreResult || fetchMoreResult.trips.rows.length === 0) {
-          return previousResult;
-        }
+class UserTripsList extends Component {
+  componentWillMount() {
+    const { subscribeToNewTrip, id } = this.props;
+    subscribeToNewTrip({ userId: id });
+  }
 
-        const rows = previousResult.trips.rows.concat(fetchMoreResult.trips.rows);
+  render() {
+    const { trips, onPress, onSharePress } = this.props;
+    return (<DataList
+      data={trips}
+      renderItem={({ item }) => (
+        <TripItem
+          key={item.id}
+          trip={item}
+          onPress={onPress}
+          onSharePress={onSharePress}
+        />
+      )}
+      fetchMoreOptions={{
+        variables: { offset: trips.rows.length },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          if (!fetchMoreResult || fetchMoreResult.trips.rows.length === 0) {
+            return previousResult;
+          }
 
-        return { trips: { ...previousResult.trips, ...{ rows } } };
-      },
-    }}
-  />
-);
+          const rows = previousResult.trips.rows.concat(fetchMoreResult.trips.rows);
+
+          return { trips: { ...previousResult.trips, ...{ rows } } };
+        },
+      }}
+    />);
+  }
+}
 
 UserTripsList.propTypes = {
+  id: PropTypes.number.isRequired,
   onPress: PropTypes.func.isRequired,
   onSharePress: PropTypes.func.isRequired,
   trips: PropTypes.shape({
     rows: PropTypes.array,
     count: PropTypes.number,
   }).isRequired,
+  subscribeToNewTrip: PropTypes.func.isRequired,
 };
 
 export default UserTripsList;
