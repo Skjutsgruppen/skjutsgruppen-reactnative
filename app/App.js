831b09533f253cca87b20716dcd002ae1e76e907
Saugat Maharjan <maharjansaugat@gmail.com>
2018-02-07 11:38:13 +0545

    added app loading screen

--- app/App.js
+++ app/App.js
@@ -6,7 +6,7 @@ import { ApolloProvider } from 'react-apollo';
 import Apollo from '@services/apollo';
 import Internet from '@components/connection/internet';
 import { PersistGate } from 'redux-persist/lib/integration/react';
-import { Loading } from '@components/common';
+import AppLoading from '@components/appLoading';
 
 const initialState = {};
 const { persistor, store } = configureStore(initialState);
@@ -14,7 +14,7 @@ const { persistor, store } = configureStore(initialState);
 const App = () => (
   <ApolloProvider store={store} client={Apollo}>
     <PersistGate
-      loading={<Loading />}
+      loading={<AppLoading />}
       persistor={persistor}
     >
       <View style={{ flex: 1 }}>
