3244ba3198c3d5c4d30ca0af09b26186df391c32
Aayush Rijal <reizal.ayz@gmail.com>
2018-04-17 17:33:08 +0545

    Localization file type migrated from js to json
    Translation in feed screen done

--- app/lang/locales/se/index.js
+++ app/lang/locales/se/index.js
@@ -1,12 +1,27 @@
-export { feed } from '@lang/locales/se/feed';
-export { global } from '@lang/locales/se/global';
-export { profile } from '@lang/locales/se/profile';
-export { group } from '@lang/locales/se/group';
-export { search } from '@lang/locales/se/search';
-export { add } from '@lang/locales/se/add';
-export { message } from '@lang/locales/se/message';
-export { trip } from '@lang/locales/se/trip';
-export { addGroup } from '@lang/locales/se/addGroup';
-export { setting } from '@lang/locales/se/setting';
-export { about } from '@lang/locales/se/about';
-export { interval } from '@lang/locales/en/interval';
+import feed from '@lang/locales/se/feed.json';
+import global from '@lang/locales/se/global.json';
+import profile from '@lang/locales/se/profile.json';
+import group from '@lang/locales/se/group.json';
+import search from '@lang/locales/se/search.json';
+import add from '@lang/locales/se/add.json';
+import message from '@lang/locales/se/message.json';
+import trip from '@lang/locales/se/trip.json';
+import addGroup from '@lang/locales/se/addGroup.json';
+import setting from '@lang/locales/se/setting.json';
+import about from '@lang/locales/se/about.json';
+import interval from '@lang/locales/se/interval.json';
+
+export {
+  feed,
+  global,
+  profile,
+  group,
+  search,
+  add,
+  message,
+  trip,
+  addGroup,
+  setting,
+  about,
+  interval,
+};
