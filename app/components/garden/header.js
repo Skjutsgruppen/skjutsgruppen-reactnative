12db5779097fbecb3704de029503a47c58e3fd1d
Saugat Maharjan <maharjansaugat@gmail.com>
2018-04-19 12:57:35 +0545

    Implement new garden UI * [x] podfile updated * [x] designed new garden header * [x] designed new garden ui * [x] designed new garden ui if supporter * [x] garden ui fixes for ios * [x] designed your suport screen * issue #379

---
+++ app/components/garden/header.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import PropTypes from 'prop-types';
+
+import { Title, Heading } from '@components/utils/texts';
+import Colors from '@theme/colors';
+import Avatar from '@components/common/avatar';
+import Curves from '@assets/curves_green.png';
+
+const styles = StyleSheet.create({
+  curves: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+  content: {
+    padding: 30,
+    paddingTop: 50,
+  },
+  heading: {
+    maxWidth: 220,
+    lineHeight: 36,
+    marginBottom: 24,
+  },
+  miniDivider: {
+    height: 1,
+    width: 64,
+    backgroundColor: Colors.text.lightGray,
+    marginTop: 24,
+    marginBottom: 32,
+  },
+});
+
+const Header = ({ showTitle, showAvatar, headingLabel, infoLabel }) => (
+  <View style={styles.wrapper}>
+    <Image source={Curves} style={styles.curves} />
+    <View style={styles.content}>
+      {
+        showTitle && <Title size={15} color={Colors.text.blue} style={{ marginVertical: 16 }}>{'TOGETHER'.toLocaleUpperCase()}</Title>
+      }
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+        <Heading
+          size={32}
+          color={Colors.text.pink}
+          fontVariation="bold"
+          style={[styles.heading, { flex: 1 }]}
+        >{headingLabel}</Heading>
+        {
+          showAvatar &&
+            <Avatar isSupporter size={120} imageURI="http://skjuts.staging.yipl.com.np/assets/img/user/1523455188140.jpg" />
+        }
+      </View>
+      <View style={styles.miniDivider} />
+      {
+        infoLabel !== '' && infoLabel && (
+          <Title size={23} color={Colors.text.gray} style={{ lineHeight: 36 }}>
+            {infoLabel}
+          </Title>
+        )
+      }
+    </View>
+  </View>
+);
+
+Header.propTypes = {
+  showAvatar: PropTypes.bool.isRequired,
+  showTitle: PropTypes.bool.isRequired,
+  headingLabel: PropTypes.string.isRequired,
+  infoLabel: PropTypes.string,
+};
+
+Header.defaultProps = {
+  infoLabel: null,
+};
+
+export default Header;
---
+++ app/components/garden/howItWorks.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+
+import { AppText, Title, Heading } from '@components/utils/texts';
+import Colors from '@theme/colors';
+import Avatar from '@components/common/avatar';
+import WateringCan from '@assets/icons/ic_watering_can.png';
+import AddPhoto from '@assets/icons/ic_add_photo.png';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: Colors.background.mutedBlue,
+    elevation: 23,
+    paddingVertical: 40,
+    paddingHorizontal: 30,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 32,
+  },
+  iconWrapper: {
+    minWidth: 64,
+  },
+  text: {
+    flex: 1,
+    marginLeft: 32,
+  },
+});
+
+const HowItWorks = () => (
+  <View style={styles.wrapper}>
+    <Title
+      size={15}
+      color={Colors.text.blue}
+      style={{ marginBottom: 16 }}
+    >THIS IS HOW IT WORKS</Title>
+    <Heading size={26} color={Colors.text.pink} style={{ lineHeight: 36 }}>
+      Everything is transparent
+    </Heading>
+    <View style={styles.row}>
+      <View style={styles.iconWrapper}>
+        <Image source={WateringCan} />
+      </View>
+      <AppText style={styles.text}>
+        All the costs are transparent.(scroll down to see them)
+      </AppText>
+    </View>
+    <View style={styles.row}>
+      <View style={styles.iconWrapper}>
+        <Avatar size={62} isSupporter imageURI="http://skjuts.staging.yipl.com.np/assets/img/user/1523455188140.jpg" />
+      </View>
+      <AppText style={styles.text}>
+        All the costs are transparent.(scroll down to see them)
+      </AppText>
+    </View>
+    <View style={styles.row}>
+      <View style={styles.iconWrapper}>
+        <Image source={AddPhoto} style={{ resizeMode: 'contain' }} />
+      </View>
+      <AppText style={styles.text}>
+        All the costs are transparent.(scroll down to see them)
+      </AppText>
+    </View>
+    <AppText style={{ lineHeight: 26, marginTop: 48 }}>
+      Your support is payed with your iTunes account or Google Play depending
+      on your device. If you like to continue to support the movement you
+      don’t have to do anything, your support will be auto-renewed.
+      You can turn of auto-renewal by going to your settings
+      in the iTunes store or Google Play.
+    </AppText>
+  </View>
+);
+
+export default HowItWorks;
