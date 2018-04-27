9310e00e92732df4399fbbaf8e2ebdbeaee8abf2
Biju Nakarmi <basnet0umesh@gmail.com>
2018-04-27 17:28:20 +0545

    * push notification implementation for ios

--- ios/skjutsgruppen/AppDelegate.h
+++ ios/skjutsgruppen/AppDelegate.h
@@ -8,8 +8,10 @@
  */
 
 #import <UIKit/UIKit.h>
+@import UserNotifications;
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UNUserNotificationCenterDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
