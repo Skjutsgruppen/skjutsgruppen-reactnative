259ff0e2de5535d19bdaba80d68ce936100210b4
Kiran Shrestha <kiran.shrestha@yipl.com.np>
2018-05-02 15:17:05 +0545

    Search Result changes - [x] Display result of public transport everyday if date is not selected - [x] Remove loading of group on screen. Added button to load more groups - [x] Public transport link and text added - [x] styled load more group

--- app/screens/Search.js
+++ app/screens/Search.js
@@ -3,11 +3,29 @@ import { withSearchAllTrips, withSearchAllGroups } from '@services/apollo/search
 import PropTypes from 'prop-types';
 import SearchResult from '@components/search/SearchResult';
 import { compose } from 'react-apollo';
+import { getDate } from '@config';
+import { FEED_TYPE_PUBLIC_TRANSPORT } from '@config/constant';
 
 const AllSearchResult = compose(withSearchAllTrips, withSearchAllGroups)(SearchResult);
 
 const Search = ({ navigation }) => {
   const { from, to, direction, filters, dates } = navigation.state.params;
+  let dateSelected = true;
+  let limit = 0;
+  let offset = 0;
+  const publicTransportSelected = filters.includes(FEED_TYPE_PUBLIC_TRANSPORT);
+
+  if (dates.length < 1) {
+    dateSelected = false;
+    if (publicTransportSelected && to.name !== '' && from.name !== '') {
+      dates.push(getDate().format('YYYY-MM-DD'));
+    }
+  }
+
+  if (!dateSelected && !publicTransportSelected) {
+    limit = 10;
+    offset = 0;
+  }
 
   return (
     <AllSearchResult
@@ -20,6 +38,9 @@ const Search = ({ navigation }) => {
       dates={dates}
       fromPlace={from}
       toPlace={to}
+      dateSelected={dateSelected}
+      limit={limit}
+      offset={offset}
     />
   );
 };
