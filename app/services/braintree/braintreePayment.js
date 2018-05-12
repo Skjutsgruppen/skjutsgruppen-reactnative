f281afeb2b2972aaa92adc15075ee6ea7af62997
Raj Krishna Maharjan <raj.maharjan33@gmail.com>
2018-05-12 10:10:16 +0545

    showPayment callback fix

--- app/services/braintree/braintreePayment.js
+++ app/services/braintree/braintreePayment.js
@@ -1,13 +1,6 @@
-import { Platform, NativeModules } from 'react-native';
+import { NativeModules } from 'react-native';
 
 export function showPayment(generateClientToken, callback) {
   NativeModules.BraintreePayment.setToken(generateClientToken);
-
-  if (Platform.OS === 'ios') {
-    NativeModules.BraintreePayment.showPayment(callback);
-  } else {
-    NativeModules.BraintreePayment.showPayment((paymentMethodNonce) => {
-      callback(null, paymentMethodNonce);
-    }, callback);
-  }
+  NativeModules.BraintreePayment.showPayment(callback);
 }
