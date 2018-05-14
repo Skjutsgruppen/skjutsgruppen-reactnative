2746a37d596574012612b03c5095f1e6e2f547cd
Umesh <basnet0umesh@gmail.com>
2018-05-14 16:44:51 +0545

    * remove secret key for android

--- android/app/src/main/java/com/skjutsgruppen/MainApplication.java
+++ android/app/src/main/java/com/skjutsgruppen/MainApplication.java
@@ -1,109 +1,96 @@
 package com.skjutsgruppen;
 
-import android.app.Application;
 import android.support.multidex.MultiDexApplication;
 
-import com.facebook.react.ReactApplication;
-import com.gettipsi.reactnativetwittersdk.TwitterReactPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import ga.piroro.rnt.RNTPackage;
-
-import com.skjutsgruppen.braintree.BraintreePaymentPackage;
-import com.tkporter.sendsms.SendSMSPackage;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.imagepicker.ImagePickerPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import com.rnfs.RNFSPackage;
-import com.evollu.react.fcm.FIRMessagingPackage;
-import com.magus.fblogin.FacebookLoginPackage;
-import com.smixx.fabric.FabricPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.rt2zz.reactnativecontacts.ReactNativeContacts;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import ga.piroro.rnt.RNTPackage;
-import com.gettipsi.reactnativetwittersdk.TwitterReactPackage;
-import com.smixx.fabric.FabricPackage;
-import com.rnfs.RNFSPackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
-import com.tkporter.sendsms.SendSMSPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.airbnb.android.react.maps.MapsPackage;
-import com.rt2zz.reactnativecontacts.ReactNativeContacts;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.crashlytics.android.Crashlytics;
+import com.evollu.react.fcm.FIRMessagingPackage;
+import com.facebook.CallbackManager;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.soloader.SoLoader;
+import com.gettipsi.reactnativetwittersdk.TwitterReactPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.magus.fblogin.FacebookLoginPackage;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.rnfs.RNFSPackage;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
+import com.skjutsgruppen.braintree.BraintreePaymentPackage;
+import com.skjutsgruppen.react.modules.GeoLocationPackage;
+import com.smixx.fabric.FabricPackage;
+import com.tkporter.sendsms.SendSMSPackage;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
 import java.util.Arrays;
 import java.util.List;
-import com.evollu.react.fcm.FIRMessagingPackage;
-import com.skjutsgruppen.react.modules.GeoLocationPackage;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+
+import ga.piroro.rnt.RNTPackage;
+import io.fabric.sdk.android.Fabric;
 
 public class MainApplication extends MultiDexApplication implements ReactApplication {
 
-  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+    private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
 
-  protected static CallbackManager getCallbackManager() {
-    return mCallbackManager;
-  }
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new RNTPackage(),
+                    new FabricPackage(),
+                    new RNFSPackage(),
+                    new RCTCameraPackage(),
+                    new LinearGradientPackage(),
+                    SendSMSPackage.getInstance(),
+                    new RNDeviceInfo(),
+                    new MapsPackage(),
+                    new ReactNativeContacts(),
+                    new VectorIconsPackage(),
+                    new ImagePickerPackage(),
+                    new ReactNativeConfigPackage(),
+                    new FacebookLoginPackage(),
+                    new RNI18nPackage(),
+                    new FIRMessagingPackage(),
+                    new TwitterReactPackage(),
+                    new GeoLocationPackage(),
+                    new FBSDKPackage(mCallbackManager),
+                    new BraintreePaymentPackage()
+            );
+        }
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
+    protected static CallbackManager getCallbackManager() {
+        return mCallbackManager;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-        new MainReactPackage(),
-        new RNTPackage(),
-        new FabricPackage(),
-        new RNFSPackage(),
-        new RCTCameraPackage(),
-        new LinearGradientPackage(),
-        SendSMSPackage.getInstance(),
-        new RNDeviceInfo(),
-        new MapsPackage(),
-        new ReactNativeContacts(),
-        new VectorIconsPackage(),
-        new ImagePickerPackage(),
-        new ReactNativeConfigPackage(),
-        new FacebookLoginPackage(),
-        new RNI18nPackage(),
-        new FIRMessagingPackage(),
-        new TwitterReactPackage(),
-        new GeoLocationPackage(),
-        new FBSDKPackage(mCallbackManager),
-        new BraintreePaymentPackage()
-      );
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected String getJSMainModuleName() {
-      return "index";
+    public void onCreate() {
+        super.onCreate();
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_CONSUMER_KEY, BuildConfig.TWITTER_CONSUMER_SECRET);
+        SoLoader.init(this, /* native exopackage */ false);
+        Fabric.with(this, new Crashlytics(), new Twitter(authConfig));
     }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
