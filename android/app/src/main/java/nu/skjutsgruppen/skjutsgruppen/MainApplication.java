e41c26a1a896accbb258533c59f49cf3a067e2f6
Anjan Shrestha <anjhero@Anjans-MacBook-Pro.local>
2018-05-28 16:28:29 +0545

    - restrict app usage if contact permission not available - show cocreatemodal after contact permission

--- android/app/src/main/java/nu/skjutsgruppen/skjutsgruppen/MainApplication.java
+++ android/app/src/main/java/nu/skjutsgruppen/skjutsgruppen/MainApplication.java
@@ -8,6 +8,7 @@ import com.airbnb.android.react.maps.MapsPackage;
 import com.crashlytics.android.Crashlytics;
 import com.facebook.CallbackManager;
 import com.facebook.react.ReactApplication;
+import com.opensettings.OpenSettingsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.levelasquez.androidopensettings.AndroidOpenSettingsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -53,6 +54,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            new OpenSettingsPackage(),
                     new RNFirebasePackage(),
                     new AndroidOpenSettingsPackage(),
                     new RNTPackage(),
